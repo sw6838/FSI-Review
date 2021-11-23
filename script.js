ee19736051456b5bf9e138ce6f33c13af31a6b9d
Shabnam Waseem <shabnamwaseem@Shabnams-MacBook-Air.local>
2021-11-22 18:53:04 -0800

    Done

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ for (let i = 0; i < container.lenght; i++) {
 
 }*/
 
+let dogVotes = 0;
+let catVotes = 0;
+let fishVotes =0;
+
 let dogDiv = document.querySelector('#doggo');
 let catDiv = document.querySelector('#catto');
 let fishDiv = document.querySelector('#fish');
@@ -16,10 +20,29 @@ let dogVotebtn = document.createElement('button');
 dogVotebtn.textContent = "Vote for Dog";
 dogDiv.append(dogVotebtn);
 
+dogVotebtn.addEventListener('click', function(){
+dogVotes = dogVotes + 1;
+document.querySelector("#doggo-votes") .textContent = dogVotes;
+localStorage.setItem('dogVotes', dogVotes)
+document.querySelector("#total-votes") .textContent = dogVotes + catVotes + fishVotes;
+})
+
 let catVotebtn = document.createElement('button');
 catVotebtn.textContent = "Vote for Cat";
 catDiv.append(catVotebtn);
 
+catVotebtn.addEventListener('click', function(){
+    catVotes = catVotes + 1;
+    document.querySelector("#catto-Votes") .textContent = catVotes;
+    document.querySelector("#total-votes") .textContent = dogVotes + catVotes + fishVotes;
+})
+
 let fishVotebtn = document.createElement('button');
 fishVotebtn.textContent = "Vote for Fish";
 fishDiv.append(fishVotebtn);
+
+fishVotebtn.addEventListener('click', function(){
+    fishVotes = fishVotes + 1;
+    document.querySelector("#fish-Votes") .textContent = fishVotes;
+    document.querySelector("#total-votes") .textContent = dogVotes + catVotes + fishVotes;
+})
